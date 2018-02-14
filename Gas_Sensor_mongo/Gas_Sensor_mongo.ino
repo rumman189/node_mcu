a0af52aaf382c509d0c523359da5f4529f2ad027
rumman189 <rumman189@gmail.com>
2018-02-15 02:53:58 +0600

    Add files via upload

---
+++ Gas_Sensor_mongo/Gas_Sensor_mongo.ino
@@ -0,0 +1,62 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+void setup() {
+ 
+  Serial.begin(9600);                                  //Serial connection
+  WiFi.begin("DataSoft_WiFi", "support123");   //WiFi connection
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+ 
+  }
+ 
+}
+ 
+void loop() {
+ 
+ if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
+    JsonObject& JSONencoder = JSONbuffer.createObject(); 
+
+    float sensorVoltage; 
+  float sensorValue;
+  sensorValue = analogRead(A0);
+  sensorVoltage = sensorValue/1024*3.3;
+ 
+    JSONencoder["sensorType"] = "Temperature";
+    JSONencoder["value"] = sensorVoltage;
+    JSONencoder["timestamp"] = "111111000";
+ 
+    
+ 
+   
+    char JSONmessageBuffer[300];
+    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+    Serial.println(JSONmessageBuffer);
+
+ 
+   HTTPClient http;    //Declare object of class HTTPClient
+ 
+   http.begin("http://192.168.6.145:5000/sensor");      //Specify request destination
+   http.addHeader("Content-Type", "application/json");  //Specify content-type header
+ 
+   int httpCode = http.POST(JSONmessageBuffer);   //Send the request
+   String payload = http.getString();                  //Get the response payload
+ 
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+ 
+   http.end();  //Close connection
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(3000);  //Send a request every 30 seconds
+ 
+}
